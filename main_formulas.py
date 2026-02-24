e07dcb72c535c4cda731aa6ae103e7ef96c7544d
Artem Novokshonov <nov.art.ig@gmail.com>
2026-02-24 16:32:45 +0100

    Update main_formulas.py

--- main_formulas.py
+++ main_formulas.py
@@ -18,3 +18,10 @@ def chr_angle_vac(n1, _theta_cr=np.pi/4, _gamma=100, _n2=1):
   alpha2 = np.arcsin(np.sin(theta_ch - _theta_cr) * n1) # Angle between the ChR after crossing the boundary and the normal to the crystal/target surface [rad]
   chr_angle_vac = alpha2 + _theta_cr # Angle between the particle velocity and the ChR after crossing the boundary [rad]
   return chr_angle_vac
+
+def refr_ind(lmbd):
+  # Sellmeier equation - dependence of refractive index on wavelength of fused silica crystal
+  # lmbd - wavelength [um]
+  eps = 1 + 0.6961663 * lmbd**2 / (lmbd**2 - 0.0684043**2) + 0.4079426 * lmbd**2 / (lmbd**2 - 0.1162414**2) + 0.8974794 * lmbd**2 / (lmbd**2 - 9.896161**2) 
+  n = np.sqrt(eps) # refractive index
+  return n
