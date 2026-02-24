b8eee73107abb1eb1191f7b09ceaf01eea2dca4c
Artem Novokshonov <nov.art.ig@gmail.com>
2026-02-24 17:43:31 +0100

    Update main_formulas.py

--- main_formulas.py
+++ main_formulas.py
@@ -31,6 +31,7 @@ def refr_ind(lmbd):
 def peak_lmbd_on_cr_angle(theta_cr, _n2=1, _obs_angle=0.97, _gamma=100):
   beta = np.sqrt(1 - 1/_gamma**2)
   min_func1 = lambda n1: np.arcsin(_n2 * np.sin(_obs_angle - theta_cr) / n1) - np.arccos(1 / (n1 * beta)) - theta_cr
-  sol = root_scalar(min_func1(n1), bracket=[0.0, 2], method='brentq')  
-  
-  
+  sol1 = root_scalar(min_func1(n1), bracket=[0.0, 2], method='brentq')
+  min_func2 = lambda lmbd: 1 + 0.6961663 * lmbd**2 / (lmbd**2 - 0.0684043**2) + 0.4079426 * lmbd**2 / (lmbd**2 - 0.1162414**2) + 0.8974794 * lmbd**2 / (lmbd**2 - 9.896161**2) - sol**2
+  sol2 = root_scalar(min_func2(lmbd), bracket=[0.1, 2], method='brentq')
+  return sol2
